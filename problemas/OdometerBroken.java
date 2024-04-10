ec37f429bacc0c3b6d42a94e08024ecb8518cd63
Diego Alba <dayo.alba.p@gmail.com>
2024-04-10 08:37:46 -0400

    Nuevo problema

---
+++ problemas/OdometerBroken.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class OdometerBroken {
+
+    
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = 2;
+        while ((n = scan.nextInt()) >= 1) {   
+            int d = 0;
+            int st = 0;
+            int ct = 0;
+            int et = 0;
+            for (int i = 0; i < n; i++) {    
+                int s = scan.nextInt();
+                et = scan.nextInt();
+                ct = et - st;
+                st = et;
+                d = d + s*ct;
+                
+            }
+        System.out.println(d + " miles");
+        }
+        
+    }
+    
+}
